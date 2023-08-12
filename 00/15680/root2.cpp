9b17ec1145579b3f945431a09004fed65b0c5995
RootLEE2 <2rootcom4@gmail.com>
2023-08-12 19:24:06 +0900

    feat: 15680 cpp (root2)

---
+++ 00/15680/root2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+typedef long long	ll;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+void
+solve (void)
+{
+	int N;
+	cin >> N;
+	cout << (N == 0 ? "YONSEI" : "Leading the Way to the Future");
+}
+
+int
+main (void)
+{
+	fastio;
+
+	solve ();
+
+	return 0;
+}
