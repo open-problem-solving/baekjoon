e935c7c0fa09fcdbeeb164c39e02579d28265876
RootLEE2 <2rootcom4@gmail.com>
2023-08-27 20:11:37 +0900

    feat: 14581 cpp (root2)

---
+++ 01/14581/root2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+typedef long long	ll;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+void
+solve (void)
+{
+	char hong_id[21];
+	cin >> hong_id;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << hong_id << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+}
+
+int
+main (void)
+{
+	fastio;
+
+	solve ();
+
+	return 0;
+}
