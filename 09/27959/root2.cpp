802d920e3a4d9f01e87e8402a6274766e6ba709e
RootLEE2 <2rootcom4@gmail.com>
2023-09-06 12:30:58 +0900

    feat: 27959 cpp (root2)

---
+++ 09/27959/root2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+void
+solve (void)
+{
+	int N, M;
+	cin >> N >> M;
+	cout << ((N * 100 >= M) ? "Yes" : "No");
+}
+
+int
+main (void)
+{
+	fastio;
+
+	int T = 1;
+	// cin >> T;
+	while (T--)
+		solve ();
+
+	return 0;
+}
