85a9e76ddd59c8d576b7c72a84973f2666dd9a5d
RootLEE2 <2rootcom4@gmail.com>
2023-11-08 13:47:47 +0900

    feat: 30328 cpp (root2)

---
+++ 08/30328/root2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+#define LOG2_10_4 14
+#define LOG2_10_5 17
+#define LOG2_10_6 20
+
+void
+solve (void)
+{
+	int n;
+	cin >> n;
+	cout << n * 4000;
+}
+
+int
+main (void)
+{
+	fastio;
+
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve ();
+	}
+
+	return 0;
+}
