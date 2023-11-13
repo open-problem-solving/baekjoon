a0119070b31d9aded4237753b65c7c75979fa6a5
RootLEE2 <2rootcom4@gmail.com>
2023-11-13 10:47:19 +0900

    feat: Edmonds-Karp basecode (root2)

---
+++ basecode/root2/edmondskarp.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+class EdmondsKarp {
+private:
+	int num_vert;
+	vector<vector<int>> adj_list;
+	int src, sink;
+	vector<vector<int>> flow, capacity;
+	vector<int> parent;
+public:
+	EdmondsKarp (int n, int s, int e) {
+		num_vert = n;
+		src = s;
+		sink = e;
+
+		adj_list.resize (n + 1);
+		flow.resize (n + 1);
+		capacity.resize (n + 1);
+		parent.resize (n + 1);
+		for (int i = 1; i <= n; i++) {
+			flow[i].resize (n + 1);
+			capacity[i].resize (n + 1);
+		}
+	}
+
+	void
+	insert_edge (int u, int v, int cap, bool directed) {
+		adj_list[u].push_back (v);
+		adj_list[v].push_back (u);
+		capacity[u][v] += cap;
+		if (!directed) {
+			capacity[v][u] += cap;
+		}
+	}
+
+	bool
+	find_shortest_path_bfs () {
+		queue<int> q;
+		vector<bool> visited (num_vert + 1);
+
+		q.push (src);
+		visited[src] = true;
+
+		while (!q.empty ()) {
+			int curr = q.front ();
+			q.pop ();
+
+			for (int next : adj_list[curr]) {
+				if (visited[next]) {
+					continue;
+				}
+				if (capacity[curr][next] - flow[curr][next] <= 0) {
+					continue;
+				}
+
+				parent[next] = curr;
+				if (next == sink) {
+					return true;
+				}
+
+				visited[next] = true;
+				q.push (next);
+			}
+		}
+
+		return false;
+	}
+
+	int
+	get_max_flow () {
+		int u, v;
+		int max_flow = 0;
+		parent[src] = -1;
+
+		while (find_shortest_path_bfs ()) {
+			int path_flow = INT32_INF;
+			for (v = sink; v != src; v = parent[v]) {
+				u = parent[v];
+				path_flow = min (path_flow, capacity[u][v] - flow[u][v]);
+			}
+
+			for (v = sink; v != src; v = parent[v]) {
+				u = parent[v];
+				flow[u][v] += path_flow;
+				flow[v][u] -= path_flow;
+			}
+			max_flow += path_flow;
+		}
+
+		return max_flow;
+	}
+};
+
+int
+main ()
+{
+	return 0;
+}
