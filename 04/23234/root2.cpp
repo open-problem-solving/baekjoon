82b859474e032a82424560c9da997741c38c6f27
RootLEE2 <2rootcom4@gmail.com>
2023-11-30 21:01:21 +0900

    feat: 23234 cpp (root2)

---
+++ 04/23234/root2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+void
+solve (void)
+{
+	cout << "The world says hello!";
+}
+
+int
+main (void)
+{
+	fastio;
+
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve ();
+	}
+
+	return 0;
+}
