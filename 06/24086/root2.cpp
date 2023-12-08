62c5597684100f89a5a827a59e7709de94b78e3a
RootLEE2 <2rootcom4@gmail.com>
2023-12-08 23:26:07 +0900

    feat: 24086 cpp (root2)

---
+++ 06/24086/root2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+void
+solve (void)
+{
+	int A, B;
+	cin >> A >> B;
+	cout << B - A;
+}
+
+int
+main (void)
+{
+	fastio;
+
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve ();
+	}
+
+	return 0;
+}
