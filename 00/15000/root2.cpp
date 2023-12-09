5058e1fc66c427b469d90efe9793125775af2347
RootLEE2 <2rootcom4@gmail.com>
2023-12-09 22:53:06 +0900

    feat: 15000 cpp (root2)

---
+++ 00/15000/root2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+#include <string.h>
+
+void
+solve (void)
+{
+	string str;
+	cin >> str;
+	int to_upper = 'A' - 'a';
+	for (char c : str) {
+		cout << (char) (c + to_upper);
+	}
+}
+
+int
+main (void)
+{
+	fastio;
+
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve ();
+	}
+
+	return 0;
+}
