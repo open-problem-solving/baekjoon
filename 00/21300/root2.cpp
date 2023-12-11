7c4a4710dfd5c1754cb269d23bc15d672118eac8
RootLEE2 <2rootcom4@gmail.com>
2023-12-11 22:40:05 +0900

    feat: 21300 cpp (root2)

---
+++ 00/21300/root2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+#define fastio	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+#define INT32_INF 0x3f3f3f3f
+#define INT64_INF 0x3f3f3f3f3f3f3f3f
+
+void
+solve (void)
+{
+	int cumul_sum = 0, tmp;
+	for (int i = 0; i < 6; i++) {
+		cin >> tmp;
+		cumul_sum += tmp;
+	}
+	cout << 5 * cumul_sum;
+}
+
+int
+main (void)
+{
+	fastio;
+
+	int T = 1;
+	// cin >> T;
+	while (T--) {
+		solve ();
+	}
+
+	return 0;
+}
